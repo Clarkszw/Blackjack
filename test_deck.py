3b3fb4328401717a8ac573af2556df44d1f37ee5
Zhiwen Shi <clarkszw@gmail.com>
2023-03-15 14:48:58 +0100

    test for deck.py

--- test_deck.py
+++ test_deck.py
@@ -1,2 +1,26 @@
 import pytest
 from deck import Deck
+
+
+@pytest.fixture
+def dk():
+    """pytest fixture"""
+    return Deck()
+
+
+def test_show(dk):
+    """test show method"""
+    assert dk.show() == print(dk.content)
+
+
+def test_shuffle(dk):
+    """test shuffle method"""
+    dk_origin = dk.content[:]
+    dk.shuffle()
+    assert dk.content != dk_origin
+
+
+def test_hit(dk):
+    """test hit method"""
+    dk_last = dk.content[-1]
+    assert dk.hit() == dk_last
